d16b92e0b41350e39474385d53487447326e78da
EmilyJaneNilsen <emily.jane.nilsen@gmail.com>
2023-02-28 21:59:55 +0100

    add padding to upcoming text

--- src/components/ComingSoon.jsx
+++ src/components/ComingSoon.jsx
@@ -6,7 +6,7 @@ import { Newsletter } from './Newsletter'
 export function ComingSoon() {
   return (
     <div className="relative flex min-h-screen">
-      <div className="absolute inset-0 hidden sm:block">
+      <div className="absolute inset-0">
         <Image
           src="https://res.cloudinary.com/dt3k2apqd/image/upload/q_auto/Norsk%20Nett/tomas-robertson-wu_a2B7QGbQ-unsplash_apr6ne.jpg"
           alt="Indulge in the rich and diverse flavours of coffee beans from My Next Coffee"
@@ -16,20 +16,9 @@ export function ComingSoon() {
           priority
         />
       </div>
-      <div className="absolute inset-0 hidden bg-gradient-to-r from-gray-900 sm:block" />
-      <div className="absolute inset-0 sm:hidden">
-        <Image
-          src="https://res.cloudinary.com/dt3k2apqd/image/upload/q_auto/Loop%20Film/forest_zatrol.webp"
-          alt="Indulge in the rich and diverse flavours of coffee beans from My Next Coffee"
-          fill
-          className="h-full w-full object-cover"
-          unoptimized
-          priority
-        />
-      </div>
-      <div className="absolute h-[100vh] w-full bg-gradient-to-b from-stone-900 sm:hidden" />
-      <div className="absolute bottom-0 left-0 h-[100vh] w-full bg-gradient-to-t from-stone-900/90 sm:hidden" />
-      <div className="absolute inset-0 bg-stone-900/20" />
+      <div className="absolute inset-0 bg-gradient-to-t from-gray-900" />
+      <div className="absolute inset-0 bg-gray-900/20" />
+
       <main className="z-1 relative flex w-screen py-6 sm:py-12">
         <Container className="w-full">
           <div className="flex flex-col justify-between ">
@@ -42,7 +31,7 @@ export function ComingSoon() {
                   height={110}
                 />
               </div>
-              <div className="prose mx-auto my-12 mb-0 max-w-lg lg:prose-xl sm:my-24 md:my-32 ">
+              <div className="prose mx-auto mt-24 mb-0 max-w-lg lg:prose-xl sm:my-24 md:my-32 ">
                 <p className="mt-3 text-center text-2xl text-white sm:text-3xl">
                   Snart lanserer vi profesjonelle nettside- og grafisk
                   design-tjenester for små norske bedrifter og entreprenører.
