d8ed0c9dada74107571ee51779e19e93df7884c3
EmilyJaneNilsen <emily.jane.nilsen@gmail.com>
2023-02-28 21:31:11 +0100

    Add favicon

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: process.env.SITE_URL || 'https://mynextcoffee.com',
+  siteUrl: process.env.SITE_URL || 'https://norsknett.no',
   generateRobotsTxt: true, // (optional)
   // ...other options
 }
